5b818e0fbfec41351343657387d678a542546fa8
Shoaib <shoaibfaisal83@gmail.com>
2026-03-18 17:49:15 +0500

    Day 1

---
+++ Tuesday_170326_1.py
@@ -0,0 +1,127 @@
+#First program in Python
+#print("Hello, World!")
+
+#Second program in Python
+# import sys
+# print(sys.version)
+
+#Third program in Python
+# if 5 > 2:
+#     print("Five is greater than two!")
+# else:
+#     print("Five is not greater than two!")
+
+#Fourth program in Python
+# print("I am", 35, "years old.")
+
+#Fifth program in Python
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+#Sixth program in Python
+# x = 4 
+# x = "Sally"
+# print(x)
+
+#Seventh program in Python
+# x = int(3)
+# y = str(3)
+# z = float(3)
+
+# print(x)
+# print(y)
+# print(z)
+
+#Eighth program in Python
+# x = 5
+# y = "John"
+
+# print(type(x))
+# print(type(y))
+
+
+#Ninth program in Python
+# x ,y,z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+#Tenth program in Python
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+#Eleventh program in Python
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+
+# x = "Python "
+# y = "is "
+# z = "awesome "
+# print(x+ y+ z)
+
+#Twelfth program in Python
+# x = 5
+# y = "John"
+# print(x, y)
+
+#thirteenth program in Python
+
+# x = "awesome"
+# def myfunc():
+#     print("Python is " + x) 
+# myfunc()    
+
+# x = "fantastic"
+# def myfunc():
+#     x = "awesome"
+#     print("Python is " + x) 
+# myfunc()
+# print("Python is " + x)
+
+#forteenth program in Python`
+# def myfunc():
+#     global  x 
+#     x = "hello"
+#     print("Hello world")
+# myfunc()
+# print(x + " Guys", end=" ")
+# print(x + " People")
+
+
+# x = "global"
+
+# def myfunc():
+#     global x  
+#     x = "hello"
+#     print(x + " world")
+# myfunc()
+# print(x + " warming")
+
+# x = 'awesome'
+# def myfunc():
+#   x = 'fantastic'
+# myfunc()
+# print('Python is ' + x)
+
+# x = "5"
+
+# print(type(x))
+
+#fifteenth program in Python
+
+# import random
+
+# print(random.randrange(1,10))
+

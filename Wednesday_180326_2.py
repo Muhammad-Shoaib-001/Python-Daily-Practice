b89a253a86423c0b06b9cb0959fe23b70e996a7e
Shoaib <shoaibfaisal83@gmail.com>
2026-03-20 11:45:13 +0500

    Day 2-3

---
+++ Wednesday_180326_2.py
@@ -0,0 +1,118 @@
+#sixteenth program in Python
+
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+
+# a = '''Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua.'''
+# print(a)
+
+# for x in "banana":
+#   print(x)
+
+# x = "Hello Shoaib's Girl"
+# print(x)
+
+# x = "Hello World"
+# print(len(x))
+
+# x = "Hello World!"
+# print("World" in x)
+
+
+# x = "Hello World"
+# if "World" not in x:
+#     print("Yes, World is Present")
+# else:
+#     print("No, World is not Present")
+
+# x = 'Welcome'
+# print(x[3])
+
+# x = "Hello World!"
+# print(x[2:5])
+
+#Seventeenth program in Python
+
+# x = "Hello World!"
+# print(x[:5])
+
+# x = "Hello World!"
+# print(x[2:])
+
+# x = "Hello World!"
+# print(x[-5:-2])
+
+# x = "Hello wORLD"
+# print(x.upper())
+
+# x = " Hello World "
+# print(x.strip())
+
+#Eighteenth Program in Python
+
+# x = "Hello World1"
+# print(x.replace("1","!"))
+
+# x = "Hello, World"
+# print(x.split(","))
+
+# print('Welcome'.upper())
+
+#Ninteenth Program in Python
+
+# x = "Hello"
+# y = "World"
+
+# print(x +" " + y)
+
+# age = 24
+# txt = f"My name is shoaib and my age is {age}"
+# print(txt)
+
+# price = 59
+# txt = f"The price is {price:.2f} dollars"
+# print(txt)
+
+
+# price = 59
+# print(f"The price is {price * 20}$")
+
+#Twentyth Program in Python
+
+# print("We are the so-called \"Vikings\" from the North.")
+
+# print("We are the so-called \t Vikings\t from the North.")
+
+# print("We are the so-called \\ Vikings\\ from the North.")
+
+# print("We are the so-called \r Vikings\r from the North.")
+
+# print("We are the so-called \b Vikings\b from the North.")
+
+# Twentyfirst Program in Python
+
+# x = "banana"
+# for a in x:
+#     print(a)
+
+# x = "Hello World"
+# clean = x.strip().upper()
+# print(clean)
+
+# x = "  Hello  World"
+# clean = x.lstrip().lower()
+# print(clean)
+
+# x = "  Hello  World"
+# clean = x.rstrip().lower()
+# print(clean)
+
+# x = "Hello world"
+# print(x.replace("Hello","Greetings"))
+

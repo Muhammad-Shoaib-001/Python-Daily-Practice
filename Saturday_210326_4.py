1ef274b66319c024adcf963da34c9e452f2ee9fd
Shoaib <shoaibfaisal83@gmail.com>
2026-03-22 00:55:44 +0500

    Day 5

---
+++ Saturday_210326_4.py
@@ -0,0 +1,105 @@
+# x = 5
+# print(1>x<3)
+
+# x = 5
+# print(6>x and x<7)
+
+# x = 5
+
+# print(not(x > 3 and x < 10))
+
+
+# x = ["apple", "banana"]
+# y = ["apple", "banana"]
+# z = x
+
+# print(x is z)
+# print(x is y)
+# print(x == y)
+
+# x = ["apple", "banana"]
+# y = ["apple", "banana"]
+
+# print(x is not y)
+
+
+# fruits = ["apple", "banana", "cherry"]
+
+# print("banana" not in fruits)
+
+# print(6 ^ 3)
+
+# mylist1 = [1,"shoaib","male",24,True]
+# print(mylist1)
+
+# x = list((1,"shoaib","male",24,True))
+# print(x)
+
+# mylist1 = [1,"shoaib","male",24,True]
+# print(len(mylist1))
+
+# mylist = ["apple", "banana", "cherry"]
+# print(type(mylist))
+
+
+# thislist = ["apple", "banana", "cherry"]
+# if "apple" in thislist:
+#   print("Yes, 'apple' is in the fruits list")
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[-4:-1])
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# mylist1 = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# mylist1.insert(2,"watermelon")
+# print(mylist1)
+
+
+# mylist1 = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# mylist1.append("hello")
+# print(mylist1)
+
+# mylist1 = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# mylist2 = [ "banana", "cherry", "orange", "world"]
+# mylist1.extend(mylist2)
+# print(mylist1)
+
+# mylist1 = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thistuple = ( "banana", "cherry", "orange", "world")
+# mylist1.extend(thistuple)
+# print(mylist1)
+
+# mylist1 = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# mylist1.remove("apple")
+# print(mylist1)
+
+# mylist1 = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# mylist1.pop(1)
+# print(mylist1)
+
+# mylist1 = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# mylist1.pop()
+# print(mylist1)
+
+# mylist1 = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# del mylist1[1]
+# print(mylist1)
+
+# mylist1 = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# del mylist1
+# print(mylist1)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# for x in thislist:
+#   print(x)

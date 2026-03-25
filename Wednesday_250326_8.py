a11cc8a0417594334ed24ff4ffa3e23d3b668974
Shoaib Faisal <shoaibfaisal83@gmail.com>
2026-03-25 17:30:05 +0500

    Day 9

---
+++ Wednesday_250326_8.py
@@ -0,0 +1,104 @@
+# i = 1
+# while i < 10:
+#     print(i)
+#     i += 1
+
+# i  = 1 
+# while i < 20:
+#     print(i)
+#     if i == 18:
+#         break
+#     i += 1
+
+# i  = 1 
+# while i < 20:
+#     print(i)
+#     if i == 18:
+#         continue
+#     i += 1
+
+# i = 1
+# while i < 10:
+#     print(i)
+#     i += 1
+# else: print("No longer exists")
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits: print(x)
+
+# for x in 'banana': print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:  
+#     print(x) 
+#     if x == 'banana': 
+#        break
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:  
+#     if x == 'banana': 
+#        break
+# print(x) 
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     continue
+#   print(x)
+
+# for x in range(6):
+#     print(x)
+
+# for x in range(2,5):
+#     print(x)
+
+# for x in range(2, 30, 3):
+#   print(x)
+
+# for x in range(6):
+#   if x == 3: break
+#   print(x)
+# else:
+#   print("Finally finished!")
+
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+# for x in adj:
+#     for y in fruits:
+#         print(x,y)
+
+# for x in [0, 1, 2]:
+#   pass
+
+# def my_function():
+#   print("Hello from a function")
+
+# my_function()
+
+# def fahrenheit_to_celsius(fahrenheit):
+#   return ( fahrenheit - 32) * 5 / 9
+
+# print("Farhenhiet: ",fahrenheit_to_celsius(30))
+
+# def get_greetings():
+#     return "Hello, World"
+# message = get_greetings()
+# print(message)  
+
+# def get_greeting():
+#   return "Hello from a function"
+
+# print(get_greeting())
+
+# def my_function(fname):
+#   print(fname + " Refsnes")
+
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+
+# def my_function(name): # name is a parameter
+#   print("Hello", name)
+
+# my_function("Emil") # "Emil" is an argument
+

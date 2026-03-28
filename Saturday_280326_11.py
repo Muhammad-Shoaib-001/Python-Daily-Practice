225b973a4abc815eb8ff0234931aa29055d0844d
Shoaib <shoaibfaisal83@gmail.com>
2026-03-28 22:13:23 +0500

    Day 12.1

---
+++ Saturday_280326_11.py
@@ -0,0 +1,46 @@
+# try:
+#   print(x)
+# except:
+#   print("An exception occurred")
+
+# try:
+#   print(x)
+# except NameError:
+#   print("Variable x is not defined")
+# except:
+#   print("Something else went wrong")
+
+# try:
+#   print("Hello")
+# except:
+#   print("Something went wrong")
+# else:
+#   print("Nothing went wrong")
+
+# try:
+#   print(x)
+# except:
+#   print("Something went wrong")
+# finally:
+#   print("The 'try except' is finished")
+
+# try:
+#   f = open("demofile.txt")
+#   try:
+#     f.write("Lorum Ipsum")
+#   except:
+#     print("Something went wrong when writing to the file")
+#   finally:
+#     f.close()
+# except:
+#   print("Something went wrong when opening the file")
+
+# x = 1
+# if x < 10:
+#  raise Exception("x is less than 10")
+
+
+# x = "hello"
+
+# if not type(x) is int:
+#   raise TypeError("Only integers are allowed")

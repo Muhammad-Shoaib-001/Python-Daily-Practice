00cdce83f4579fb843441792094f7efa220bd442
Shoaib <shoaibfaisal83@gmail.com>
2026-04-09 16:58:54 +0500

    Day 19

---
+++ Thursday_090426_17.py
@@ -0,0 +1,102 @@
+# import re
+
+# txt = "The rain in Spain"
+# x = re.search("^The.*Spain$", txt)
+
+# print(x)
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.findall("ai", txt)
+# print(x)
+
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.findall("Portugal", txt)
+# print(x)
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.search("\s", txt)
+
+# print("The first white-space character is located in position:", x.end())
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.search("Portugal", txt)
+# print(x)
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.split("\s", txt)
+# print(x)
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.split("\s", txt, 1)
+# print(x)
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.sub("\s", "9", txt)
+# print(x)
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.sub("\s", "9", txt, 2)
+# print(x)
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.search("ai", txt)
+# print(x) #this will print an object
+
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.search(r"\bS\w+", txt)
+# print(x.span())
+
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.search(r"\bS\w+", txt)
+# print(x.string)
+
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.search(r"\bS\w+", txt)
+# print(x.group())
+
+# import camelcase
+
+# c = camelcase.CamelCase()
+
+# txt = "hello world"
+
+# print(c.hump(txt))
+
+# x = -1
+
+# if x < 0:
+#   raise Exception("Sorry, no numbers below zero")
+
+# x = "hello"
+
+# if not type(x) is int:
+#   raise TypeError("Only integers are allowed")
+

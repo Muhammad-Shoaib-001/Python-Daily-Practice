7f03203f5dd6ab39eeebbeadb83246a61e6bdc3e
Shoaib <shoaibfaisal83@gmail.com>
2026-04-10 23:26:26 +0500

    Day 20

---
+++ Friday_100426_18.py
@@ -0,0 +1,109 @@
+# txt = f"The price is 49 dollars"
+# print(txt)
+
+# price = 59
+# txt = f"The price is {price} dollars"
+# print(txt)
+
+# price = 59
+# txt = f"The price is {price:.2f} dollars"
+# print(txt)
+
+# txt = f"The price is {95:.2f} dollars"
+# print(txt)
+
+# txt = f"The price is {20 * 59} dollars"
+# print(txt)
+
+# price = 59
+# tax = 0.25
+# txt = f"The price is {price + (price * tax)} dollars"
+# print(txt)
+
+# price = 49
+# txt = f"It is very {'Expensive' if price>50 else 'Cheap'}"
+
+# print(txt)
+
+
+# fruit = "apples"
+# txt = f"I love {fruit.upper()}"
+# print(txt)
+
+# def myconverter(x):
+#   return x * 0.3048
+
+# txt = f"The plane is flying at a {myconverter(30000)} meter altitude"
+# print(txt)
+
+# price = 59000
+# txt = f"The price is {price:,} dollars"
+# print(txt)
+
+# price = 49
+# txt = "The price is {} dollars"
+# print(txt.format(price))
+
+# quantity = 3
+# itemno = 567
+# price = 49
+# myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+# print(myorder.format(quantity, itemno, price))
+
+# quantity = 3
+# itemno = 567
+# price = 49
+# myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+# print(myorder.format(quantity, itemno, price))
+
+# age = 36
+# name = "John"
+# txt = "His name is {1}. {1} is {0} years old."
+# print(txt.format(age, name))
+
+# myorder = "I have a {carname}, it is a {model}."
+# print(myorder.format(carname = "Ford", model = "Mustang"))
+
+# x = None
+# print(x)
+
+# x = None
+# print(type(x))
+
+# result = None
+# if result is None:
+#   print("No result yet")
+# else:
+#   print("Result is ready")
+
+
+# print(bool(None))
+
+# def myfunc():
+#   x = 5
+
+# x = myfunc()
+# print(x)
+
+# print("Enter your name: ")
+# name = input()
+# print(f"Hello, {name}")
+# import math
+# x = input("Enter a number:")
+
+# #find the square root of the number:
+# y = math.sqrt(float(x))
+
+# print(f"The square root of {x} is {y}")
+
+# y = True
+# while y == True:
+#   x = input("Enter a number:")
+#   try:
+#     x = float(x);
+#     y = False
+#   except:
+#     print("Wrong input, please try again.")
+
+# print("Thank you!")
+
